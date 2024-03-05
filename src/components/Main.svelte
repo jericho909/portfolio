2040f673647ef4c9caee663f0e24490337e954af
jericho909 <omerahmetyucel@gmail.com>
2024-03-05 22:55:02 +0300

    initial commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"> 
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Ömer Ahmet</span> Yücel.<br/> Full Stack <span class="poppins text-violet-400">Developer.</span>
+            </h2>
+            <p class="text-base sm:text-lg md.text-xl">
+                My <span class="text-violet-400">favorite tech</span> includes Java (Spring Boot), JavaScript(SvelteKit or React), TailwindCSS, PostgreSQL!
+            </p>
+            <a href="https://www.linkedin.com/in/omerahmetyucel/" target="_blank" class="blueShadow mx-auto lg:auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 rounded-full bg-white text-slate-950 cursor-pointer">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+
+                </div>
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </a>
+        </div>
+    </section>
+</main>
